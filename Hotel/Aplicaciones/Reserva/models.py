3438bd3fff20d0caa119cf8a2c47a3b6dd94ec62
zeroxc23 <116192239+zeroxc23@users.noreply.github.com>
2024-04-12 10:01:35 -0500

    Display

--- Hotel/Aplicaciones/Reserva/models.py
+++ Hotel/Aplicaciones/Reserva/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
+from Aplicaciones.Habitaciones.models import Habitaciones
 from django.db import models
 
-# Create your models here.
+
+
+class Reservacion(models.Model):
+    id_reserva = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Id_habitacion = models.ForeignKey(Habitaciones, on_delete=models.CASCADE)
+    fechaentrada = models.DateField()
+    fechasalida = models.DateField()
+    cantidad_Niños = models.IntegerField()
+    cantidad_Adultos = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    POR_CONFIRMAR = 'Por Confirmar'
+    CONFIRMADA = 'Confirmada'
+    CANCELADA = 'Cancelada'
+    ESTADO_CHOICES = [
+        (POR_CONFIRMAR, 'Por Confirmar'),
+        (CONFIRMADA, 'Confirmada'),
+        (CANCELADA, 'Cancelada')
+    ]
+    
+    estado = models.CharField(max_length=25, choices=ESTADO_CHOICES, default=POR_CONFIRMAR)
